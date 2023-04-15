4f18fd03d8c8af081a6e115d53b64270d0801951
maxwellkjr <maxwellkjr@gmail.com>
2023-04-16 01:30:23 +0200

    Basic layout for the Home Screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,37 @@
-import { Text, View } from 'react-native'
+import { SafeAreaView, Text, View } from 'react-native'
+import Header from '../components/Header'
 
 export default function Page() {
   return (
-    <View className="flex flex-col h-screen items-start p-4 justify-center bg-white">
-      <View>
-        <Text className="text-black font-black text-4xl">Hello World</Text>
-        <Text>This is the first page of your app.</Text>
+    <SafeAreaView>
+      {/* Greetings Section */}
+      <View className="flex flex-col p-4 justify-center bg-pink-600">
+        <View className="flex flex-col py-4 mt-10">
+          <Text className="text-white text-lg">Hello,</Text>
+          <Text className="text-white font-black text-3xl">Maxwell.</Text>
+        </View>
+
+        {/* Reminder Section */}
+        <View className="mt-4 px-4 py-6 bg-pink-500 rounded-2xl">
+          <View className="">
+            <Text className="font-bold text-xl text-white">Alright, what is on your mind?</Text>
+            <Text className="text-white text-md">
+              Remember, lists are things you care about at the moment.
+            </Text>
+          </View>
+        </View>
+
+        <View className="mt-5">
+          <View>
+            <Text className="font-bold text-white text-2xl mb-4">Current Lists</Text>
+            <View>
+              <View className="p-4 bg-pink-800 rounded-2xl">
+                <Text className="text-white">Call Her</Text>
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   )
 }
---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Header() {
+  return (
+    <View>
+      <Text className="font-serif">Hello,</Text>
+      <Text className="font-bold text-4xl">Maxwell</Text>
+    </View>
+  )
+}
